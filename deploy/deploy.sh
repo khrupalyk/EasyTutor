8c1e0bdfe80738ea47e01b76b3836ee4a8b97c3e
Andriy <khrupalik@gmail.com>
2015-09-03 22:25:02 +0300

    Added glassfish-web.xml

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,2 @@
 scp -i /home/saigak/.ssh/key.pem /home/saigak/EasyTutor/target/easytutor.war ec2-user@ec2-54-68-142-11.us-west-2.compute.amazonaws.com:/home/ec2-user/
-ssh -i /home/saigak/.ssh/key.pem ec2-user@ec2-54-68-142-11.us-west-2.compute.amazonaws.com 'cd /home/ec2-user/glassfish4/glassfish/bin/; ./asadmin undeploy easytutor;' 
-ssh -i /home/saigak/.ssh/key.pem ec2-user@ec2-54-68-142-11.us-west-2.compute.amazonaws.com 'cd /home/ec2-user/glassfish4/glassfish/bin/; ./asadmin deploy /home/ec2-user/easytutor.war;'
+ssh -i /home/saigak/.ssh/key.pem ec2-user@ec2-54-68-142-11.us-west-2.compute.amazonaws.com 'cd /home/ec2-user/glassfish4/glassfish/bin/; ./asadmin undeploy easytutor; ./asadmin deploy /home/ec2-user/easytutor.war;'
